0f653e169784539552f7698734ec944fc663317c
tausif40 <tausif6040@gmail.com>
2024-12-27 13:42:44 +0530

    add time

--- src/component/FastResult/FastResult.js
+++ src/component/FastResult/FastResult.js
@@ -22,12 +22,16 @@ const ScheduleCard = ({ title, timeLabel, time, result, refresh }) => {
 				<h2 className="text-white text-2xl font-bold uppercase tracking-wider mb-1 drop-shadow-md">
 					{title}
 				</h2>
-				<p className="text-gray-100 font-semibold text-2xl py-3">
-					{result ? result : '_ _ _ _ _'}
-				</p>
+				<div className='flex items-center justify-between text-[#58111a] font-medium md:px-5'>
+					<p>{title === 'Hadoti Day' ? '11:45 AM' : title === 'Hadoti Night' && '6:45 PM'}</p>
+					<p className="text-gray-100 font-semibold text-2xl py-3">
+						{result ? result : '_ _ _ _ _'}
+					</p>
+					<p>{title === 'Hadoti Day' ? '12:45 PM' : title === 'Hadoti Night' && '7:45 PM'}</p>
+				</div>
 				<div className='hidden sm:block'>
 					<button
-						className='absolute top-4 right-4 shadow-sm px-2 py-1 rounded-md bg-[#931c2c] hover:bg-[#6e1d4f] text-white font-light text-sm flex gap-2 items-center transition-all'
+						className='absolute top-4 right-4 shadow-sm px-2 py-1 rounded-md bg-[#b82641] hover:bg-[#6e1d4f] text-white font-light text-sm flex gap-2 items-center transition-all'
 						onClick={handleRefresh}
 					>
 						<p> {isRefreshing ? 'Refreshing..' : 'Refresh'}</p>
@@ -37,7 +41,7 @@ const ScheduleCard = ({ title, timeLabel, time, result, refresh }) => {
 
 				<div className='sm:hidden block'>
 					<button
-						className='m-auto mt-2 shadow-sm px-2 py-1 rounded-md bg-[#931c2c] hover:bg-[#6e1d4f] text-white font-light text-sm flex gap-2 items-center transition-all'
+						className='m-auto mt-2 shadow-sm px-2 py-1 rounded-md bg-[#b82641] hover:bg-[#6e1d4f] text-white font-light text-sm flex gap-2 items-center transition-all'
 						onClick={handleRefresh}
 					>
 						<p> {isRefreshing ? 'Refreshing..' : 'Refresh'}</p>
